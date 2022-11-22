07c84ce9951de9a057c0a13cb6edcd61b3ca3df0
George Panaretos <georgepanaretos.cac@gmail.com>
2022-11-22 11:58:01 +0100

    Adding rest service functions

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,20 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+//import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor(private http: HttpClient) { }
-    private apiServerUrl = '';
+  private apiServerUrl = '';
 
-    /**
-      * Get Employees
-    */
+  constructor(private http: HttpClient) { }
+
+  /**
+    * Get Employees
+  */
   public getEmployees(): Observable<any> {
     return this.http.get<any>(`${this.apiServerUrl}/employee/all`);
   }
+
+  /**
+    * Add Employees
+  */
+  //  public addEmployees(): Observable<any> {
+  //   return this.http.post<any>(`${this.apiServerUrl}/employee/add`, employee);
+  // }
+
+  // /**
+  //   * Update Employees
+  // */
+  //  public updateEmployees(): Observable<any> {
+  //   return this.http.put<any>(`${this.apiServerUrl}/employee/update`, employee);
+  // }
+
+  // /**
+  //   * Delete Employees
+  // */
+  //  public deleteEmployees(): Observable<void> {
+  //   return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  // }
 
 }
