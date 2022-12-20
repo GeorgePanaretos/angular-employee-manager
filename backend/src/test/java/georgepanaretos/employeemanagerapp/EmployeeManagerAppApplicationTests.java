6e685c97a37e8421527d95b082f7d8ca44470c73
George Panaretos <georgepanaretos.cac@gmail.com>
2022-12-20 17:13:58 +0100

    Backend service

---
+++ backend/src/test/java/georgepanaretos/employeemanagerapp/EmployeeManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package georgepanaretos.employeemanagerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
